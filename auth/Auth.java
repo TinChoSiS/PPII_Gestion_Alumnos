ce2b8a6cbc39262b025535f8a61f361280b6879b
Martín Acosta <m@sis.uy>
2023-05-29 23:28:24 -0300

    initial

---
+++ auth/Auth.java
@@ -0,0 +1,40 @@
+package auth;
+
+import DB.DBController;
+import models.Usuario;
+import utils.Utils;
+
+public class Auth {
+
+    public static Usuario login() {
+        // Scanner userFile = new Scanner(new File(usuariosPath));
+        DBController dbCtrl = new DBController();
+
+        int tryCount = 0;
+
+        Utils.println();
+        do {
+            tryCount++;
+
+            Utils.print("Nombre de Usuario: ");
+            String username = Utils.next();
+
+            Utils.print("Contraseña: ");
+            String password = Utils.next();
+
+            Usuario user = dbCtrl.userLogin(username, password);
+
+            
+            if (user != null) {
+                Utils.print(user != null ? "Bienvenido " + user.getName() + "\n" : "");
+                return user;
+            }
+            
+            Utils.println("Usuario o contraseña incorrectos. Intentos restantes: " + (3 - tryCount));
+        } while (tryCount < 3);
+
+        System.exit(0);
+        return null;
+
+    }
+}
